05d983c03e7714274fc7a768a5e6a5274a9e991b
Leonardo Dognani <roberto.leonardo@venturus.org.br>
2023-01-05 09:50:17 -0300

    switchs

--- script.js
+++ script.js
@@ -243,6 +243,7 @@ if (favourite === 13) {
 
 //BOOLEAN OPERATORS
 
+/*
 const hasDriversLicense = true;
 const hasGoodVision = true;
 
@@ -266,4 +267,30 @@ if (hasDriversLicense && hasGoodVision && !isTired) {
   console.log("Usagi is able to drive!");
 } else {
   console.log("Someone else should drive...");
+}
+*/
+
+const day = "monday";
+
+switch (day) {
+  case "monday":
+    console.log("Plan course structure");
+    console.log("Go to coding meetup");
+    break;
+  case "tuesday":
+    console.log("Prepare theory videos");
+    break;
+  case "wednesday":
+  case "thursday":
+    console.log("Write code examples");
+    break;
+  case "friday":
+    console.log("Record videos");
+    break;
+  case "saturday":
+  case "sunday":
+    console.log("Enjoy the weekend");
+    break;
+  default:
+    console.log("Not a valid day!");
 }
